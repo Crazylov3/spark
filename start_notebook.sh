aa5dde1615537564937e1bdb9d4cbca2bdd86a02
crazylov3 <giangletruong2444@gmail.com>
2023-02-01 18:35:32 +0700

    update

--- start_notebook.sh
+++ start_notebook.sh
@@ -1,1 +1,2 @@
-jupyter notebook --ip 0.0.0.0 --port 8888 --no-browser --allow-root
+
+jupyter notebook -NotebookApp.token=f9a3bd4e9f2c3be01cd629154cfb224c2703181e050254b5 --ip 0.0.0.0 --port 8888 --no-browser --allow-root
